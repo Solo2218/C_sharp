53f2c3da52f46621b9972057eb09beeb240f6ffe
Konstantin Solovei <SoloveiKonstantin@gmail.com>
2023-10-12 19:13:46 +0300

    add Example 004 Condition If-Else

---
+++ Example005_ConditionsIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользовотеля: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Solo")
+{
+    Console.WriteLine("Ура, это же SOLO!");    
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
